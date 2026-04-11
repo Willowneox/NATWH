e0b97fe4d828d1759cdb155699cae8d097bad074
KrazyJax <165222334+KrazyJax@users.noreply.github.com>
2026-04-10 22:52:46 -0700

    trash click-to-pickup minigame complete

--- Assets/Scripts/MinigameTrash.cs
+++ Assets/Scripts/MinigameTrash.cs
@@ -4,17 +4,24 @@ using UnityEngine.UI;
 
 public class MinigameTrash : MonoBehaviour
 {
-    public Button trashPrefab;
+    public GameObject trashPrefab;
 
     public int numOfTrash = 4;
     private void Start()
     {
         for (int i = 0; i < numOfTrash; i++)
         {
-            int x_rand = Random.Range(0, 1920);
-            int y_rand = Random.Range(0, 1080);
+            int x_rand = Random.Range(-960,960);
+            int y_rand = Random.Range(-540,540);
 
-            Instantiate(trashPrefab, new Vector3(x_rand, y_rand, 0), Quaternion.identity);
+            GameObject btn = Instantiate(trashPrefab, gameObject.transform);
+            RectTransform rect = btn.GetComponent<RectTransform>();
+            rect.localPosition = Vector3.zero;
+            rect.localScale = Vector3.one;
+
+            rect.anchoredPosition = new Vector2 (x_rand, y_rand);
+
+            btn.GetComponent<Button>().onClick.AddListener(() => PickupTrash(btn));
         }
     }
 
