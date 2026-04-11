3ad9d82ace63836f5795396bc77dd9f34ab85cd0
KrazyJax <165222334+KrazyJax@users.noreply.github.com>
2026-04-11 00:11:45 -0700

    charging station and minigames are functional

--- Assets/Scripts/ChargingStation.cs
+++ Assets/Scripts/ChargingStation.cs
@@ -4,8 +4,23 @@ public class ChargingStation : MonoBehaviour
 {
     public Shop shop;
 
+    public bool isOnTrigger { get; private set; } = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) isOnTrigger = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) isOnTrigger = false;
+    }
     private void OnMouseDown()
     {
+        if (isOnTrigger)
+        {
+            shop.OpenShop();
+            Debug.Log("yes we are openeing the shop");
+        }
         shop.OpenShop();
         Debug.Log("yes we are openeing the shop");
     }
