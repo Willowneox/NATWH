57a31a58ef2d1e560d559d89b89fe96c7a1bbfea
David-Li372 <138244633+David-Li372@users.noreply.github.com>
2026-04-10 23:23:25 -0700

    plant minigame wip

---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Blade : MonoBehaviour
+{
+    private bool cutting;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Collider bladeCollider;
+    public float minVelo = 0.1f;
+    private Vector2 direction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+    }
+    private void OnEnable(){
+        StopCutting();
+    }
+    private void OnDisable(){
+        StopCutting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            StartCutting();
+        }else if (Input.GetMouseButtonUp(0)){
+            StopCutting();
+        }else if (cutting){
+            ContCutting();
+        }
+    }
+
+    private void StartCutting(){
+        Vector2 newPosition = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = newPosition;
+
+        cutting = true;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopCutting(){
+        cutting = false;
+        bladeCollider.enabled = true;
+    }
+
+    private void ContCutting(){
+        Vector2 newPosition = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        direction = newPosition - (Vector2)transform.position;
+
+        float velo = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velo > minVelo;
+        
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/PlantMiniGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlantMiniGame : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    private void OnEnable(){
+
+    }
+    private void OnDisable(){
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
