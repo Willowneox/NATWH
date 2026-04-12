28c7d842bf0a4e11d39da08d1440adc771fe6c33
Michael Schneider <michaews@uci.edu>
2026-04-12 15:42:43 -0700

    Added props script

---
+++ Assets/Scripts/Props.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Props : Obstacle
+{
+    PolygonCollider2D polygonCollider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = availableSprites[Random.Range(0, availableSprites.Length)];
+        polygonCollider = GetComponent<PolygonCollider2D>();
+        SyncCollider();
+
+        int dir = Random.Range(1, 3);
+        if (dir == 2)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D player)
+    {
+        // don't damage the player
+        return;    
+    }
+
+    // Makes the collider match the sprite shape
+    private void SyncCollider()
+    {
+        polygonCollider.pathCount = spriteRenderer.sprite.GetPhysicsShapeCount();
+        List<Vector2> path = new List<Vector2>();
+
+        for (int i = 0; i < polygonCollider.pathCount; i++)
+        {
+            path.Clear();
+            spriteRenderer.sprite.GetPhysicsShape(i, path);
+            polygonCollider.SetPath(i, path.ToArray());
+        }
+    }
+}
