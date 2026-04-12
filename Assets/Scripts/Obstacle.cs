244fb3e7fa912c0bbabdfdc18f1e5f2bf2b9f9b7
Michael Schneider <michaews@uci.edu>
2026-04-12 15:43:29 -0700

    Updated protection tm

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public Sprite[] availableSprites;
-    private SpriteRenderer spriteRenderer;
+    protected SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
