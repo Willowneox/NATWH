6db82e9c3c46b21e994d3143bb3428087ac382b6
KrazyJax <165222334+KrazyJax@users.noreply.github.com>
2026-04-11 19:15:37 -0700

    updated sprites, start on room gen

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] private Door _doorNorth;
+    [SerializeField] private Door _doorSouth;
+    [SerializeField] private Door _doorEast;
+    [SerializeField] private Door _doorWest;
+
+    [SerializeField] private Transform _trashContainer;
+    [SerializeField] private BoxCollider2D _trashSpawnArea;
+    [SerializeField] private GameObject _trashPrefab;
+    [SerializeField] private int _minTrash = 3;
+    [SerializeField] private int _maxTrash = 8;
+    [SerializeField] private float _trashCheckRadius = 0.5f;
+    [SerializeField] private int _maxPlacementAttempts = 10;
+
+    public bool IsGenerated { get; private set; } = false;
+
+    private void Awake()
+    {
+        _doorNorth.OwnerRoom = this;
+        _doorSouth.OwnerRoom = this;
+        _doorEast.OwnerRoom = this;
+        _doorWest.OwnerRoom = this;
+    }
+
+    public void Generate(DoorDirection? sharedDoorDirection = null)
+    {
+        if (IsGenerated) return;
+        IsGenerated = true;
+
+        SpawnTrash();
+
+        if (sharedDoorDirection.HasValue)
+            GetDoor(sharedDoorDirection.Value).Open();
+    }
+
+    public Door GetDoor(DoorDirection direction)
+    {
+        return direction switch
+        {
+            DoorDirection.North => _doorNorth,
+            DoorDirection.South => _doorSouth,
+            DoorDirection.East => _doorEast,
+            DoorDirection.West => _doorWest,
+            _ => null
+        };
+    }
+
+    private void SpawnTrash()
+    {
+        int count = Random.Range(_minTrash, _maxTrash + 1);
+        Bounds bounds = _trashSpawnArea.bounds;
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int attempt = 0; attempt < _maxPlacementAttempts; attempt++)
+            {
+                Vector2 randomPos = new Vector2(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    Random.Range(bounds.min.y, bounds.max.y));
+
+                if (Physics2D.OverlapCircle(randomPos, _trashCheckRadius) == null)
+                {
+                    Instantiate(_trashPrefab, randomPos, Quaternion.identity, _trashContainer);
+                    break;
+                }
+            }
+        }
+    }
+}
